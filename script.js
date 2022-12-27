ec0d88130fc4fd49432129a86ce55db8a53a8645
emin <eminkhidirov98@gmail.com>
2022-12-27 20:43:47 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+//let incr = 10,
+    //decr = 10;
+
+   //++incr;
+   //--decr;
+
+    //console.log(++incr);
+    //console.log(--decr);
+
+    //console.log(51%2)
+    //console.log(2*4 === 8);
+
+    const isChecked = false
+    ;
+    const isClose = false
+    ;
+
+    console.log(isChecked || !isClose);
